c7f739621deac7009e10fd00c6619928948d9fe4
vierra4 <149682182+vierra4@users.noreply.github.com>
2025-06-15 22:13:30 +0000

    nothing

--- core/signals.py
+++ core/signals.py
@@ -1,7 +1,7 @@
 # core/signals.py
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CustomUser, Driver, Client
+from .models import CustomUser, Driver, Client, DemoRequest
 from django.conf import settings
 from django.core.mail import send_mail
 
